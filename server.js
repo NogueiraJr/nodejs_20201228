b9f9478b34273f9cedda06adde523777b132120d
Nogueira <anogueirajr@gmail.com>
2020-12-28 16:07:35 -0300

    Tratamento de erros no Servidor

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-'use strict'
+'use strict' //Força a screver código JS com mais qualidade
 
-//Importando os
+//Importando os pacotes necessários para a Aplicação
 const http = require('http');
 const debug = require('debug')('nodestr:server');
 const express = require('express');
@@ -22,8 +22,9 @@ const route = router.get('/', (req, res, next) => {
 
 app.use('/', route);
 
-//Ouvir a porta
-server.listen(port);
+//Rodando a aplicação
+server.listen(port); //Ouvindo a Porta da Aplicação
+server.on('error', onError); //Tratando o erro ocorrido no Server.
 console.log('API rodando na porta ' + port);
 
 //Normalizar a porta do server
@@ -40,3 +41,36 @@ function normalizePort(val) {
 
     return false;
 }
+
+//Tratando erros do/no Servidor HTTP
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    //https://nodejs.org/api/errors.html#errors_error_syscall
+    switch (error.code) {
+        case 'EACCES':
+            console.log('[' + bind + '] Permission denied');
+            process.exit(1);
+            break;
+
+        case 'EADDRINUSE':
+            console.log('[' + bind + '] Address already in use');
+            process.exit(1);
+            break;
+
+        case 'ECONNREFUSED':
+            console.log('[' + bind + '] Connection refused');
+            process.exit(1);
+            break;
+
+        default:
+            break;
+    }
+}
+
