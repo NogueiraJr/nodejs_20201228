bc7e222183b7c7b766bccb80f1254506878fad9a
Nogueira <anogueirajr@gmail.com>
2020-12-28 15:46:50 -0300

    Normalizando a porta do serviço

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const debug = require('debug')('nodestr:server');
 const express = require('express');
 
 const app = express();
-const port = 3000; // Em produção tem de ser uma porta que esteja livre.
+const port = normalizePort(process.env.PORT || '3000'); //process.env.PORT é do AZURE
 app.set('port', port);
 
 const server = http.createServer(app);
@@ -16,7 +16,7 @@ const router = express.Router();
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: 'Node Store API',
-        version: '0.0.1'
+        version: '0.0.2'
     });
 });
 
@@ -25,3 +25,18 @@ app.use('/', route);
 //Ouvir a porta
 server.listen(port);
 console.log('API rodando na porta ' + port);
+
+//Normalizar a porta do server
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+
+    if (port >= 0) {
+        return port;
+    }
+
+    return false;
+}
