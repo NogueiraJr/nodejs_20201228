62d9c40d7ce971b7dd8212a66458e88355e12599
Nogueira <anogueirajr@gmail.com>
2020-12-28 18:06:14 -0300

    Método POST

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //Rota criada
 const route = router.get('/', (req, res, next) => {
@@ -12,6 +16,12 @@ const route = router.get('/', (req, res, next) => {
         version: '0.0.3'
     });
 });
+
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
 app.use('/', route); //Rota atribuida ao App
+app.use('/create', create);
 
 module.exports = app;
