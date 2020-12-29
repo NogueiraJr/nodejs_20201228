e72de8f660f22149ac8f767d7b517869b09e9755
Nogueira <anogueirajr@gmail.com>
2020-12-29 11:59:08 -0300

    Isolando a rota inicial em um Index

--- src/app.js
+++ src/app.js
@@ -6,16 +6,11 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+//Carregando as rotas aqui
+const index = require('./routes/index');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-//Rota criada
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: 'Node Store API',
-        version: '0.0.3'
-    });
-});
 
 const ins = router.post('/', (req, res, next) => {
     res.status(201).send(req.body);
@@ -33,7 +28,7 @@ const del = router.delete('/', (req, res, next) => {
     res.status(200).send(req.body);
 });
 
-app.use('/', route); //Rota atribuida ao App
+app.use('/', index); //Rota atribuida ao App
 app.use('/products', ins);
 app.use('/products', upd);
 app.use('/products', del);
