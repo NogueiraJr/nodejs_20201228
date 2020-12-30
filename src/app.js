f33b80048bead53741e4bfe79a10b1426b1fc840
Nogueira <anogueirajr@gmail.com>
2020-12-30 20:10:31 -0300

    Acessando o MongoDb

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+//Conectar Banco de Dados
+mongoose.connect('mongodb://usrapi:UserApi123@192.168.15.7:27017');
+
 
 //Carregando as rotas aqui
 const indexRoute = require('./routes/index-route');
