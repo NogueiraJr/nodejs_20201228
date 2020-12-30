f346324f77e0af224b752c97296bf32992369c0c
Nogueira <anogueirajr@gmail.com>
2020-12-30 19:15:02 -0300

    Isolando o operacional

--- src/app.js
+++ src/app.js
@@ -7,30 +7,13 @@ const app = express();
 const router = express.Router();
 
 //Carregando as rotas aqui
-const index = require('./routes/index');
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/products-route');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const ins = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-
-const upd = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
-
-const del = router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
-
-app.use('/', index); //Rota atribuida ao App
-app.use('/products', ins);
-app.use('/products', upd);
-app.use('/products', del);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 module.exports = app;
