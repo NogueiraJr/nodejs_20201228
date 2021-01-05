39cee68bdc1c4f3bf3023c382b445ce42682e2ef
Nogueira <anogueirajr@gmail.com>
2021-01-05 18:54:06 -0300

    Alterando um Produto por Id

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -88,11 +88,23 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
+    Product
+        .findByIdAndUpdate(req.params.id, {
+            $set: {
+                title: req.body.title,
+                description: req.body.description,
+                slug: req.body.slug,
+                price: req.body.price
+            }
+        })
+        .then(x => {
+            res.status(200).send({ message: 'Produto atualizado com sucesso' });
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Falha ao atualizar o produto',
+                data: e
+            });
+        });
 };
 
 exports.delete = (req, res, next) => {
