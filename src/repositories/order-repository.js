e5794d05390e6edb47149fc84d0fe9150d82e031
Nogueira <anogueirajr@gmail.com>
2021-01-06 20:50:36 -0300

    GET personalizado do Pedido (Order)

--- src/repositories/order-repository.js
+++ src/repositories/order-repository.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const Order = mongoose.model('Order');
 
 exports.get = async () => {
-    const res = await Order.find({});
+    const res = await Order
+        .find({}, 'number status customer items')
+        .populate('customer', 'name')
+        .populate('items.product', 'title');
     return res;
 }
 
