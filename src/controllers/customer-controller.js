2860bfaa3ae6fb5aa18ead4f173cd2720a7e1ce5
Nogueira <anogueirajr@gmail.com>
2021-01-08 20:20:27 -0300

    Gravando a senha criptografada (MD5) no banco

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -2,6 +2,7 @@
 
 const ValidationContract = require('../validators/validator');
 const repository = require('../repositories/customer-repository');
+const md5 = require('md5');
 
 exports.get = async (req, res, next) => {
     try {
@@ -26,7 +27,11 @@ exports.post = async (req, res, next) => {
     }
 
     try {
-        await repository.create(req.body)
+        await repository.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: md5(req.body.password + global.SALT_KEY)
+        })
         res.status(201).send({ message: 'Cliente cadastrado com sucesso' });
     } catch (e) {
         res.status(400).send(e);
