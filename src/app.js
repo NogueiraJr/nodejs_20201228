2e78840d2e863a2b5aa7930058372485695877d3
Nogueira <anogueirajr@gmail.com>
2021-01-08 20:08:49 -0300

    Criando o arquivo de configuração

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const config = require('./config');
 
 const app = express();
 const router = express.Router();
 
 //Conectar Banco de Dados
-mongoose.connect('mongodb://usrapi:UserApi123@192.168.15.7:27017');
+mongoose.connect(config.connectionString);
 
 //Carregar os Modelos
 const Product = require('./models/product');
