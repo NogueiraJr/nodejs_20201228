225c362a563202cef951277dab5d3e24c3730ab4
Nogueira <anogueirajr@gmail.com>
2021-05-03 07:47:58 -0300

    Convert config to ES6 module

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
-module.exports = {
-    connectionString: 'mongodb://usrapi:UserApi123@192.168.15.7:27017',
-    sendgridKey: 'SUA CHAVE',
-    containerConnectionString: 'SUA CONNECTION STRING'
-}
+export const connectionString = 'mongodb://usrapi:UserApi123@192.168.15.7:27017';
+export const sendgridKey = 'SUA CHAVE';
+export const containerConnectionString = 'SUA CONNECTION STRING';
