d64c49f907755fa5a5fa51d88a44a97373523f80
Nogueira <anogueirajr@gmail.com>
2021-05-12 09:58:40 -0300

    Arquivo morto do Cliente.

---
+++ src/models/cliente/cliente.js
@@ -0,0 +1,41 @@
+'use strict'
+
+import { Schema as _Schema, model } from 'mongoose';
+const Schema = _Schema;
+
+//Modelo irá atender o Arquivo Morto do Cadastro do Cliente.
+
+const schema = new Schema({
+    // o MonboDb com o Mongoose cria um _id automaticamente aqui como chave geral
+    customer: {
+        type: _Schema.Types.ObjectId,
+        ref: 'Log'
+    },
+    id: {
+        type: Number,
+        required: true,
+    },
+    //Último usuário dono do Cliente.
+    usuarioId: {
+        type: Number,
+        required: true,
+    },
+    //Nome do Cliente
+    nome: {
+        type: String,
+        required: true
+    },
+    //Celuar do Cliente
+    celular: {
+        type: String,
+        required: true
+    },
+    //Email do Cliente
+    email: {
+        type: String,
+        required: true
+    },
+});
+
+export default model('Client', schema);
+//
